86a0d23b950091b48b8af7d376335914ed111674
ZhukovMikhail <79285451+ZhukovMikhail@users.noreply.github.com>
2021-07-02 00:35:55 +0300

    js task 02 done
    js task 02 done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,31 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+// const item1 = document.createElement('li');
+// item1.textContent = ingredients[0];
+
+// const item2 = document.createElement('li');
+// item2.textContent = ingredients[1];
+
+// const item3 = document.createElement('li');
+// item3.textContent = ingredients[2];
+
+// const item4 = document.createElement('li');
+// item4.textContent = ingredients[3];
+
+// const item5 = document.createElement('li');
+// item5.textContent = ingredients[4];
+
+// const item6 = document.createElement('li');
+// item6.textContent = ingredients[5];
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+const listEl = ingredients.map((el) => {
+const listItemtEl = document.createElement('li');
+listItemtEl.textContent = `${el}`;
+return listItemtEl;
+});
+
+ingredientsEl.append(...listEl);
